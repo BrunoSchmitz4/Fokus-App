1cae53fc1ca42309bb43fab99e55db7b32c8727f
Bruno_Ss <brunosch250@gmail.com>
2025-04-23 12:39:23 -0300

    Update

--- app/index.jsx
+++ app/index.jsx
@@ -5,24 +5,24 @@ import { ActionButton } from "../components/ActionButton"
 import { Timer } from "../components/Timer"
 import { Footer } from "../components/Footer"
 import { DataAtual } from "../components/DataAtual"
-
+import { IconPlay, IconPause } from "../components/Icons"
 
 const pomodoro = [
     {
         id: 'focus',
-        initialValue: 25,
+        initialValue: 25 * 60,
         image: require('./Imagem Foco.png'),
         display: 'Foco'
     },
     {
         id: 'short',
-        initialValue: 5,
+        initialValue: 5 * 60,
         image: require('./Imagem Descanso Curto.png'),
         display: 'Pausa curta'
     },
     {
         id: 'long',
-        initialValue: 15,
+        initialValue: 15 * 60,
         image: require('./Imagem Descanso Longo.png'),
         display: 'Pausa longa'
 
@@ -33,19 +33,51 @@ export default function Index() {
     
     const [timerType, setTimerType] = useState(pomodoro[0])
 
+    const [seconds, setSeconds] = useState(pomodoro[0].initialValue)
+
+    const [timerRunning, setTimerRunning] = useState(false)
+
     const timerRef = useRef(null)
 
+
+    // Faz a limpeza com um double Check (limpa intervalo, ref atual e para o timer)
+    const clear = () => {
+        if (timerRef.current != null) {
+            clearInterval(timerRef.current)
+            timerRef.current = null
+            setTimerRunning(false)
+        } 
+    }
+
+    // Troca o tipo do timer (foco, pausa longa e curta, definindo novo tipo de timer, reseta contador de segundos e limpa intervalo)
+    const toggleTimerType = (newTimerType) => {
+        setTimerType(newTimerType)
+        setSeconds(newTimerType.initialValue)
+        clear()
+    }
+
+    // Olha o tipo de timer atual 
     const toggleTimer = () => {
         if (timerRef.current) {
-            
-            clearInterval(timerRef.current)
-            return
+          clear()
+          return
         }
+      
+        setTimerRunning(true)
+      
         const id = setInterval(() => {
-            console.log('Timer rolando')
-        }, 1000)
-        timerRef.current = id
-    }
+          setSeconds(oldState => {
+              if (oldState === 0) {
+                  clear()
+                  return timerType.initialValue
+              }
+              return oldState -1
+              })
+        }, 1000);
+        
+        timerRef.current = id;
+      };
+    
     return (
         <View style= {styles.container}>
             <DataAtual />
@@ -56,14 +88,15 @@ export default function Index() {
                         <ActionButton
                             key={p.id} 
                             active={ timerType.id === p.id }
-                            onPress={ () => setTimerType(p)}
+                            onPress={ () => toggleTimerType(p)}
                             display={p.display}
                         />
                     ))}
                 </View>
-                <Timer totalSeconds={timerType.initialValue}/>
+                <Timer totalSeconds={seconds}/>
                 <FokusButton 
-                    title={timerRef.current ? 'Pausar' : 'Começar'}
+                    title={timerRunning? 'Pausar' : 'Começar'}
+                    icon={timerRunning? <IconPause /> : <IconPlay />}
                     onPress={toggleTimer}    
                 />
             </View>
