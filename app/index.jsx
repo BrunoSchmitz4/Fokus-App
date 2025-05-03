790c95e84a44d172120e221a9df13b3028564a34
Bruno_Ss <brunosch250@gmail.com>
2025-05-03 15:43:36 -0300

    Create react-router navigation

--- app/index.jsx
+++ app/index.jsx
@@ -1,106 +1,20 @@
-import { Image, StyleSheet, View } from "react-native";
-import { useRef, useState } from "react";
-import { FokusButton } from "../components/FokusButton";
-import { ActionButton } from "../components/ActionButton"
-import { Timer } from "../components/Timer"
-import { Footer } from "../components/Footer"
-import { DataAtual } from "../components/DataAtual"
-import { IconPlay, IconPause } from "../components/Icons"
-
-const pomodoro = [
-    {
-        id: 'focus',
-        initialValue: 25 * 60,
-        image: require('./Imagem Foco.png'),
-        display: 'Foco'
-    },
-    {
-        id: 'short',
-        initialValue: 5 * 60,
-        image: require('./Imagem Descanso Curto.png'),
-        display: 'Pausa curta'
-    },
-    {
-        id: 'long',
-        initialValue: 15 * 60,
-        image: require('./Imagem Descanso Longo.png'),
-        display: 'Pausa longa'
-
-    },
-]
+import { Text, View, StyleSheet, Image } from "react-native"
+import { FokusButton } from '../components/FokusButton'
+import { Footer } from '../components/Footer'
+import { Link } from "expo-router"
 
 export default function Index() {
-    
-    const [timerType, setTimerType] = useState(pomodoro[0])
-
-    const [seconds, setSeconds] = useState(pomodoro[0].initialValue)
-
-    const [timerRunning, setTimerRunning] = useState(false)
-
-    const timerRef = useRef(null)
-
-
-    // Faz a limpeza com um double Check (limpa intervalo, ref atual e para o timer)
-    const clear = () => {
-        if (timerRef.current != null) {
-            clearInterval(timerRef.current)
-            timerRef.current = null
-            setTimerRunning(false)
-        } 
-    }
-
-    // Troca o tipo do timer (foco, pausa longa e curta, definindo novo tipo de timer, reseta contador de segundos e limpa intervalo)
-    const toggleTimerType = (newTimerType) => {
-        setTimerType(newTimerType)
-        setSeconds(newTimerType.initialValue)
-        clear()
-    }
-
-    // Olha o tipo de timer atual 
-    const toggleTimer = () => {
-        if (timerRef.current) {
-          clear()
-          return
-        }
-      
-        setTimerRunning(true)
-      
-        const id = setInterval(() => {
-          setSeconds(oldState => {
-              if (oldState === 0) {
-                  clear()
-                  return timerType.initialValue
-              }
-              return oldState -1
-              })
-        }, 1000);
-        
-        timerRef.current = id;
-      };
-    
     return (
-        <View style= {styles.container}>
-            <DataAtual />
-            <Image source={timerType.image} />
-            <View style = {styles.actions}>
-                <View style={styles.context}>
-                    {pomodoro.map(p => (
-                        <ActionButton
-                            key={p.id} 
-                            active={ timerType.id === p.id }
-                            onPress={ () => toggleTimerType(p)}
-                            display={p.display}
-                        />
-                    ))}
-                </View>
-                <Timer totalSeconds={seconds}/>
-                <FokusButton 
-                    title={timerRunning? 'Pausar' : 'Começar'}
-                    icon={timerRunning? <IconPause /> : <IconPlay />}
-                    onPress={toggleTimer}    
-                />
+        <View style={styles.container}>
+            <Image source={require('../assets/images/FokusLogo.svg')} />
+            <View style={styles.inner}>
+                <Text style={styles.title}>Otimize sua {'\n'}produtividade, {'\n'}
+                    <Text style={styles.Bold}>mergulhe no que {'\n'}importa</Text>
+                </Text>
+                <Image source={ require('../assets/images/Imagem tela inicial.svg') }/>
+                <FokusButton title="Quero iniciar!" onPress={() => console.log('navegar ->')}/>
+                <Footer />
             </View>
-            <Footer />
         </View>
     )
 }
@@ -113,20 +27,15 @@ const styles = StyleSheet.create({
         alignItems: "center",
         gap: 40
     },
-
-    actions: {
-        paddingVertical: 24,
-        paddingHorizontal: 24,
-        backgroundColor: '#14448080',
-        width: '80%',
-        borderRadius: 32,
-        borderWidth: 2,
-        borderColor: '#144480',
-        gap: 32
+    inner: {
+        gap: 16
     },
-    context: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center'
+    title: {
+        color: '#FFFF',
+        textAlign: 'center',
+        fontSize: 26
+    },
+    Bold: {
+        fontWeight: 'bold'
     }
 })
---
+++ app/pomodoro.jsx
@@ -0,0 +1,132 @@
+import { Image, StyleSheet, View } from "react-native";
+import { useRef, useState } from "react";
+import { FokusButton } from "../components/FokusButton";
+import { ActionButton } from "../components/ActionButton"
+import { Timer } from "../components/Timer"
+import { Footer } from "../components/Footer"
+import { DataAtual } from "../components/DataAtual"
+import { IconPlay, IconPause } from "../components/Icons"
+
+const pomodoro = [
+    {
+        id: 'focus',
+        initialValue: 25 * 60,
+        image: require('../assets/images/Imagem Foco.png'),
+        display: 'Foco'
+    },
+    {
+        id: 'short',
+        initialValue: 5 * 60,
+        image: require('../assets/images/Imagem Descanso Curto.png'),
+        display: 'Pausa curta'
+    },
+    {
+        id: 'long',
+        initialValue: 15 * 60,
+        image: require('../assets/images/Imagem Descanso Longo.png'),
+        display: 'Pausa longa'
+
+    },
+]
+
+export default function Pomodoro() {
+    
+    const [timerType, setTimerType] = useState(pomodoro[0])
+
+    const [seconds, setSeconds] = useState(pomodoro[0].initialValue)
+
+    const [timerRunning, setTimerRunning] = useState(false)
+
+    const timerRef = useRef(null)
+
+
+    // Faz a limpeza com um double Check (limpa intervalo, ref atual e para o timer)
+    const clear = () => {
+        if (timerRef.current != null) {
+            clearInterval(timerRef.current)
+            timerRef.current = null
+            setTimerRunning(false)
+        } 
+    }
+
+    // Troca o tipo do timer (foco, pausa longa e curta, definindo novo tipo de timer, reseta contador de segundos e limpa intervalo)
+    const toggleTimerType = (newTimerType) => {
+        setTimerType(newTimerType)
+        setSeconds(newTimerType.initialValue)
+        clear()
+    }
+
+    // Olha o tipo de timer atual 
+    const toggleTimer = () => {
+        if (timerRef.current) {
+          clear()
+          return
+        }
+      
+        setTimerRunning(true)
+      
+        const id = setInterval(() => {
+          setSeconds(oldState => {
+              if (oldState === 0) {
+                  clear()
+                  return timerType.initialValue
+              }
+              return oldState -1
+              })
+        }, 1000);
+        
+        timerRef.current = id;
+      };
+    
+    return (
+        <View style= {styles.container}>
+            <DataAtual />
+            <Image source={timerType.image} />
+            <View style = {styles.actions}>
+                <View style={styles.context}>
+                    {pomodoro.map(p => (
+                        <ActionButton
+                            key={p.id} 
+                            active={ timerType.id === p.id }
+                            onPress={ () => toggleTimerType(p)}
+                            display={p.display}
+                        />
+                    ))}
+                </View>
+                <Timer totalSeconds={seconds}/>
+                <FokusButton 
+                    title={timerRunning? 'Pausar' : 'Começar'}
+                    icon={timerRunning? <IconPause /> : <IconPlay />}
+                    onPress={toggleTimer}    
+                />
+            </View>
+            <Footer />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#021123',
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 40
+    },
+
+    actions: {
+        paddingVertical: 24,
+        paddingHorizontal: 24,
+        backgroundColor: '#14448080',
+        width: '80%',
+        borderRadius: 32,
+        borderWidth: 2,
+        borderColor: '#144480',
+        gap: 32
+    },
+    context: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    }
+})
